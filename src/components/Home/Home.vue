d884419ad84cd0b5935f05890946a19f7251f6d1
renfuwei <1147907177@qq.com>
2018-11-23 15:18:08 +0800

    ren04-增加home和header和footer组件并配置home路由

---
+++ src/components/Home/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div>
+      我是home主页
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  components: {},
+  // props,
+  data () {
+    return {}
+  },
+  created () {},
+  mounted () {},
+  activited () {},
+  updated () {},
+  beforeRouteUpdate () {},
+  filter: {},
+  computed: {},
+  watch: {},
+  methods: {}
+}
+</script>
+<style lang="less" scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,28 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
+// 引入组件
+// import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/Home/Home' // 引入Home组件
 
 Vue.use(Router)
 
+// 路由配置
 export default new Router({
   routes: [
+    // {
+    //   path: '/',
+    //   name: 'HelloWorld',
+    //   component: HelloWorld
+    // },
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      redirect: '/Home'
+    },
+    {
+      path: '/Home',
+      name: 'Home',
+      component: Home
     }
   ]
 })
