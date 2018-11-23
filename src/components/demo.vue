06165c1fb0a88472cb60bed8bdf11728e43697c2
renfuwei <1147907177@qq.com>
2018-11-23 13:41:34 +0800

    ren02-增加demo.vue组件模板

---
+++ src/components/demo.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+  </div>
+</template>
+<script>
+  export default {
+    components: {},
+    props,
+    data () {
+      return {}
+    },
+    created () {},
+    mounted () {},
+    activited () {},
+    updated () {},
+    beforeRouteUpdate () {},
+    filter: {},
+    computed: {},
+    watch: {},
+    methods: {}
+  }
+</script>
+<style lang="less" scoped>
+
+</style>
+
+
